eb2e559d844252e62ebfc65ed18ae6f55fe4fd7a
Wouter Huybrechts <wouter.huybrechts@student.kuleuven.ne>
2021-03-29 21:30:16 +0200

    Add tabs, add design

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from tkinter import *
+from ui import Application
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Window:
+
+    def __init__(self):
+        self.root = Tk()
+        self.root.title("The Storehouse")
+        self.root.configure(bg='#000000')
+        self.root.geometry('1280x720')
+        self.root.focus_force()
+
+        self.menu = Application(self.root)
+
+    def update(self):
+        self.root.update()
+        self.root.update_idletasks()
+
+        # self.menu.update()
+        # self.canvas.update()
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    window = Window()
+    while True:
+        window.update()
